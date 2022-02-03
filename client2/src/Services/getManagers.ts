caad3fbab4600cf85a5688a87f22f040e7aea4a0
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-02-03 09:13:41 +0100

    deleting unecessary files

--- client2/src/Services/getManagers.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const getManagers = async () => {
-    const res = await axios.get('http://localhost:3000/api/admin/getAllManagers')
-    return res
-}
-
-export {getManagers} 
--- client2/src/components/manageManagers/CreateManager.tsx
+++
@@ -1,7 +0,0 @@
-import {FC} from 'react';
-
-const CreateManager:FC = () => {
-  return <div>createmanager</div>;
-};
-
-export default CreateManager;
